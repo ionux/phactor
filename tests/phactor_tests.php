6ce70eb1450b050c82852714f9d2fa290c36ae81
Rich Morgan <ionux@users.noreply.github.com>
2015-03-19 19:49:38 -0400

    Adding more tests

--- tests/phactor_tests.php
+++ tests/phactor_tests.php
@@ -18,11 +18,31 @@ class PhactorTest extends \PHPUnit_Framework_TestCase
 
     public function testKeyCreation()
     {
-        $key = new Key;
-
+        $key  = new Key;
         $info = $key->GenerateKeypair();
 
         $this->assertNotNull($info);
     }
 
+    public function testSinCreation()
+    {
+        $key  = new Key;
+        $info = $key->GenerateKeypair();
+
+        $sin   = new \Phactor\Sin;
+        $sinfo = $sin->Generate($info['public_key_compressed']));
+
+        $this->assertNotNull($sinfo);
+    }
+
+    public function testSignatureCreation()
+    {
+        $key  = new Key;
+        $info = $key->GenerateKeypair();
+
+        $sig   = new \Phactor\Signature;
+        $sigfo = $sig->generate('my message to sign...', $info['private_key_hex']);
+  
+        $this->assertNotNull($sigfo);
+    }
 }
