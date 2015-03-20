af65bde70da62988d6c51aa4a50ecf016cb400e2
Rich Morgan <ionux@users.noreply.github.com>
2015-03-20 12:17:27 -0400

    Update phactor_tests.php

--- tests/phactor_tests.php
+++ tests/phactor_tests.php
@@ -4,6 +4,9 @@
  * These tests are a work in progress. If you have ideas
  * for additional or improved test cases, please submit
  * a pull request.
+ *
+ * Thanks,
+ * Rich <rich@bitpay.com>
  */
 
 namespace Tests;
