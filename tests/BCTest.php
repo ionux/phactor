37603cb5c415825f0f5c4c4cf5b1d5470a43bd84
Rich Morgan <ionux@users.noreply.github.com>
2015-07-18 11:43:09 -0400

    Bug fixes

--- tests/BCTest.php
+++ tests/BCTest.php
@@ -47,7 +47,7 @@ class BCTest extends \PHPUnit_Framework_TestCase
         $this->a = '957620976190666461915977492034526193591830013034186215918313385644855166379351262190562120407134214207526691350895955';
         $this->b = '157663433277362577011687738037596860467660792611897958941147717548714000615442450180007858983049321228191510557052';
 
-        $bc = new \Phactor\BC;
+        $this->bc = new \Phactor\BC;
     }
 
     public function testBcAdd()
@@ -57,7 +57,7 @@ class BCTest extends \PHPUnit_Framework_TestCase
 
         $expected_result = '957778639623943824492989179772563790452297673826798113877254533362403880379966704640742128266117263528754882861453007';
 
-        $result = $bc->add($this->a, $this->b);
+        $result = $this->bc->add($this->a, $this->b);
 
         $this->assertEquals($result, $expected_result);
     }
@@ -69,7 +69,7 @@ class BCTest extends \PHPUnit_Framework_TestCase
 
         $expected_result = '957463312757389099338965804296488596731362352241574317959372237927306452378735819740382112548151164886298499840338903';
 
-        $result = $bc->sub($this->a, $this->b);
+        $result = $this->bc->sub($this->a, $this->b);
 
         $this->assertEquals($result, $expected_result);
     }
@@ -81,7 +81,7 @@ class BCTest extends \PHPUnit_Framework_TestCase
 
         $expected_result = '150981810884639958700328085278405718749810206599288911717839892598416430383239698022998119007526637510937085529989130930737456136347911513955480178694591753112850253972637216399933791443453571052105672003128661792601294741343524660';
 
-        $result = $bc->mul($this->a, $this->b);
+        $result = $this->bc->mul($this->a, $this->b);
 
         $this->assertEquals($result, $expected_result);
     }
@@ -93,7 +93,7 @@ class BCTest extends \PHPUnit_Framework_TestCase
 
         $expected_result = '6073';
 
-        $result = $bc->div($this->a, $this->b);
+        $result = $this->bc->div($this->a, $this->b);
 
         $this->assertEquals($result, $expected_result);
     }
@@ -105,7 +105,7 @@ class BCTest extends \PHPUnit_Framework_TestCase
 
         $expected_result = '130945897243531723997858932200459971726019502129911268723296971515040641769262247374392803075686388719647737919159';
 
-        $result = $bc->mod($this->a, $this->b);
+        $result = $this->bc->mod($this->a, $this->b);
 
         $this->assertEquals($result, $expected_result);
     }
@@ -118,7 +118,7 @@ class BCTest extends \PHPUnit_Framework_TestCase
 
         $expected_result = '1';
 
-        $result = $bc->comp($this->a, $this->b);
+        $result = $this->bc->comp($this->a, $this->b);
 
         $this->assertEquals($result, $expected_result);
     }
@@ -130,7 +130,7 @@ class BCTest extends \PHPUnit_Framework_TestCase
 
         $expected_result = '16320031509886753001468114610224267914757061072957414462029557633094547166069133482767770945598158313244622262371';
 
-        $result = $bc->inv($this->a, $this->b);
+        $result = $this->bc->inv($this->a, $this->b);
 
         $this->assertEquals($result, $expected_result);
     }
@@ -143,7 +143,7 @@ class BCTest extends \PHPUnit_Framework_TestCase
 
         $expected_result = '917037934040364982737244091531827293772718769617550961534802228981137468765677724565272391346532251949140929536931667224208094060917023615329647508125531895055074287250548098811179279399871619095442945851773292458773975997781235362025';
 
-        $result = $bc->power($this->a, '2');
+        $result = $this->bc->power($this->a, '2');
 
         $this->assertEquals($result, $expected_result);
     }
