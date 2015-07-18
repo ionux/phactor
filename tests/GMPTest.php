6ac2bf7de91acb443cb014b4b0d7452a8ebf34ef
Rich Morgan <ionux@users.noreply.github.com>
2015-07-18 11:42:35 -0400

    Refactor to improve code quality

--- tests/GMPTest.php
+++ tests/GMPTest.php
@@ -27,12 +27,15 @@ class GMPTest extends \PHPUnit_Framework_TestCase
 {
     protected $a;
     protected $b;
+    protected $gmp;
 
     public function setUp()
     {
         // Two randomly generated numers for our math functions.
         $this->a = '957620976190666461915977492034526193591830013034186215918313385644855166379351262190562120407134214207526691350895955';
         $this->b = '157663433277362577011687738037596860467660792611897958941147717548714000615442450180007858983049321228191510557052';
+
+        $this->gmp = new \Phactor\GMP;
     }
 
     public function testGmpAdd()
@@ -40,13 +43,9 @@ class GMPTest extends \PHPUnit_Framework_TestCase
         // Test that our GMP calls are returning
         // the correct result for addition.
 
-        $gmp = new \Phactor\GMP;
-
         $expected_result = '957778639623943824492989179772563790452297673826798113877254533362403880379966704640742128266117263528754882861453007';
-        $a = $this->a;
-        $b = $this->b;
 
-        $result = $gmp->add($a, $b);
+        $result = $this->gmp->add($this->a, $this->b);
 
         $this->assertEquals($result, $expected_result);
     }
@@ -56,13 +55,9 @@ class GMPTest extends \PHPUnit_Framework_TestCase
         // Test that our GMP calls are returning
         // the correct result for subtraction.
 
-        $gmp = new \Phactor\GMP;
-
         $expected_result = '957463312757389099338965804296488596731362352241574317959372237927306452378735819740382112548151164886298499840338903';
-        $a = $this->a;
-        $b = $this->b;
 
-        $result = $gmp->sub($a, $b);
+        $result = $this->gmp->sub($this->a, $this->b);
 
         $this->assertEquals($result, $expected_result);
     }
@@ -72,13 +67,9 @@ class GMPTest extends \PHPUnit_Framework_TestCase
         // Test that our GMP calls are returning
         // the correct result for multiplication.
 
-        $gmp = new \Phactor\GMP;
-
         $expected_result = '150981810884639958700328085278405718749810206599288911717839892598416430383239698022998119007526637510937085529989130930737456136347911513955480178694591753112850253972637216399933791443453571052105672003128661792601294741343524660';
-        $a = $this->a;
-        $b = $this->b;
 
-        $result = $gmp->mul($a, $b);
+        $result = $this->gmp->mul($this->a, $this->b);
 
         $this->assertEquals($result, $expected_result);
     }
@@ -88,13 +79,9 @@ class GMPTest extends \PHPUnit_Framework_TestCase
         // Test that our GMP calls are returning
         // the correct result for division.
 
-        $gmp = new \Phactor\GMP;
-
         $expected_result = '6073';
-        $a = $this->a;
-        $b = $this->b;
 
-        $result = $gmp->div($a, $b);
+        $result = $this->gmp->div($this->a, $this->b);
 
         $this->assertEquals($result, $expected_result);
     }
@@ -104,13 +91,9 @@ class GMPTest extends \PHPUnit_Framework_TestCase
         // Test that our GMP calls are returning
         // the correct result for 'a' modulo 'b'.
 
-        $gmp = new \Phactor\GMP;
-
         $expected_result = '130945897243531723997858932200459971726019502129911268723296971515040641769262247374392803075686388719647737919159';
-        $a = $this->a;
-        $b = $this->b;
 
-        $result = $gmp->mod($a, $b, false);
+        $result = $this->gmp->mod($this->a, $this->b, false);
 
         $this->assertEquals($result, $expected_result);
     }
@@ -121,13 +104,9 @@ class GMPTest extends \PHPUnit_Framework_TestCase
         // the correct result for comparing two
         // arb precision values.
 
-        $gmp = new \Phactor\GMP;
-
         $expected_result = '1';
-        $a = $this->a;
-        $b = $this->b;
 
-        $result = $gmp->comp($a, $b);
+        $result = $this->gmp->comp($this->a, $this->b);
 
         $this->assertEquals($result, $expected_result);
     }
@@ -137,13 +116,9 @@ class GMPTest extends \PHPUnit_Framework_TestCase
         // Test that our GMP calls are returning
         // the correct result for inverse modulo.
 
-        $gmp = new \Phactor\GMP;
-
         $expected_result = '16320031509886753001468114610224267914757061072957414462029557633094547166069133482767770945598158313244622262371';
-        $a = $this->a;
-        $b = $this->b;
 
-        $result = $gmp->inv($a, $b);
+        $result = $this->gmp->inv($this->a, $this->b);
 
         $this->assertEquals($result, $expected_result);
     }
@@ -154,12 +129,9 @@ class GMPTest extends \PHPUnit_Framework_TestCase
         // the correct result for raising a number
         // to a power.
 
-        $gmp = new \Phactor\GMP;
-
         $expected_result = '917037934040364982737244091531827293772718769617550961534802228981137468765677724565272391346532251949140929536931667224208094060917023615329647508125531895055074287250548098811179279399871619095442945851773292458773975997781235362025';
-        $a = $this->a;
 
-        $result = $gmp->power($a, '2');
+        $result = $this->gmp->power($this->a, '2');
 
         $this->assertEquals($result, $expected_result);
     }
@@ -170,12 +142,9 @@ class GMPTest extends \PHPUnit_Framework_TestCase
         // the correct result for normalizing a
         // value into a string.
 
-        $gmp = new \Phactor\GMP;
-
         $expected_result = 'string';
-        $a = $this->a;
 
-        $result = gettype($gmp->normalize($a));
+        $result = gettype($this->gmp->normalize($this->a));
 
         $this->assertEquals($result, $expected_result);
     }
