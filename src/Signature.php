e5301638b59ccb9241caf11c98c4efaeb42faae2
Rich Morgan <ionux@users.noreply.github.com>
2015-11-07 14:39:15 -0500

    Corrected email address

--- src/Signature.php
+++ src/Signature.php
@@ -3,7 +3,7 @@
  * This file is part of the Phactor PHP project. You can always find the latest
  * version of this class and project at: https://github.com/ionux/phactor
  *
- * Copyright (c) 2015 Rich Morgan, rich@bitpay.com
+ * Copyright (c) 2015 Rich Morgan, rich@richmorgan.me
  *
  * The MIT License (MIT)
  *
@@ -31,7 +31,7 @@ namespace Phactor;
  * This class implements the elliptic curve math functions required to generate
  * an ECDSA signature based on a previously generated private key.
  *
- * @author Rich Morgan <rich@bitpay.com>
+ * @author Rich Morgan <rich@richmorgan.me>
  */
 final class Signature
 {
