ba04a89c2662a4409ae763aa282f1951cd59b11b
Rich Morgan <ionux@users.noreply.github.com>
2015-11-07 14:38:44 -0500

    Corrected email address

--- src/Point.php
+++ src/Point.php
@@ -3,7 +3,7 @@
  * This file is part of the Phactor PHP project. You can always find the latest
  * version of this class and project at: https://github.com/ionux/phactor
  *
- * Copyright (c) 2015 Rich Morgan, rich@bitpay.com
+ * Copyright (c) 2015 Rich Morgan, rich@richmorgan.me
  *
  * The MIT License (MIT)
  *
@@ -31,7 +31,7 @@ namespace Phactor;
  * This trait implements the elliptic curve math functions required to generate
  * new EC points based on the secp256k1 curve parameters.
  *
- * @author Rich Morgan <rich@bitpay.com>
+ * @author Rich Morgan <rich@richmorgan.me>
  */
 trait Point
 {
