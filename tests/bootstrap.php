a326f21bb67538710e7e15105c9df8928cefdb73
Rich Morgan <ionux@users.noreply.github.com>
2015-11-07 14:35:15 -0500

    Added standard test header comments

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,12 @@
 <?php
 
+/**
+ * These tests are a work in progress. If you have ideas
+ * for additional or improved test cases, please submit
+ * a pull request.
+ *
+ * Thanks,
+ * Rich <rich@richmorgan.me>
+ */
+ 
 require 'vendor/autoload.php';
