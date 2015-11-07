20172c626ccd61b215a1251f104d8df020330683
Rich Morgan <ionux@users.noreply.github.com>
2015-11-07 14:39:40 -0500

    Corrected email address

--- src/Sin.php
+++ src/Sin.php
@@ -3,7 +3,7 @@
  * This file is part of the Phactor PHP project. You can always find the latest
  * version of this class and project at: https://github.com/ionux/phactor
  *
- * Copyright (c) 2015 Rich Morgan, rich@bitpay.com
+ * Copyright (c) 2015 Rich Morgan, rich@richmorgan.me
  *
  * The MIT License (MIT)
  *
@@ -31,7 +31,7 @@ namespace Phactor;
  * This class generates Service Identification Numbers (SINs) based on the
  * Identity Protocol v1 spec, @see: https://en.bitcoin.it/wiki/Identity_protocol_v1
  *
- * @author Rich Morgan <rich@bitpay.com>
+ * @author Rich Morgan <rich@richmorgan.me>
  */
 final class Sin
 {
